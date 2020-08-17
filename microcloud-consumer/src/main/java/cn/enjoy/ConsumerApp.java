578a6bc6c4dbc7c7c6397de94129fa8ab2654ba3
Administrator <Administrator@DESKTOP-NP2DBR7>
2020-08-17 14:07:50 +0800

    Spring Cloud Feign practice finished

--- microcloud-consumer/src/main/java/cn/enjoy/ConsumerApp.java
+++ microcloud-consumer/src/main/java/cn/enjoy/ConsumerApp.java
@@ -2,9 +2,11 @@ package cn.enjoy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients("cn.enjoy.service")
 public class ConsumerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class,args);
--- microcloud-consumer/src/main/java/cn/enjoy/controller/ConsumerProductController.java
+++ microcloud-consumer/src/main/java/cn/enjoy/controller/ConsumerProductController.java
@@ -1,6 +1,7 @@
 package cn.enjoy.controller;
 
 
+import cn.enjoy.service.IProductClientService;
 import cn.enjoy.vo.Product;
 
 import org.springframework.cloud.client.ServiceInstance;
@@ -25,16 +26,20 @@ public class ConsumerProductController {
     @Resource
     private LoadBalancerClient loadBalancerClient;
     
-    @Resource
-    private RestTemplate restTemplate;
+//    @Resource
+//    private RestTemplate restTemplate;
+
+//    @Resource
+//    private HttpHeaders httpHeaders;
 
     @Resource
-    private HttpHeaders httpHeaders;
-
+    private IProductClientService iProductClientService;
+    
     @RequestMapping("/product/get")
     public Object getProduct(long id) {
-        Product product = restTemplate.exchange(PRODUCT_GET_URL + id,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), Product.class).getBody();
-        return  product;
+//        Product product = restTemplate.exchange(PRODUCT_GET_URL + id,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), Product.class).getBody();
+//        return  product;
+        return  iProductClientService.getProduct(id);
     }
 
     @RequestMapping("/product/list")
@@ -44,13 +49,15 @@ public class ConsumerProductController {
                 "【*** ServiceInstance ***】host = " + serviceInstance.getHost()
                         + "、port = " + serviceInstance.getPort()
                         + "、serviceId = " + serviceInstance.getServiceId());
-        List<Product> list = restTemplate.exchange(PRODUCT_LIST_URL,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), List.class).getBody();
-        return  list;
+//        List<Product> list = restTemplate.exchange(PRODUCT_LIST_URL,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), List.class).getBody();
+//        return  list;
+        return iProductClientService.listProduct();
     }
 
     @RequestMapping("/product/add")
     public Object addPorduct(Product product) {
-        Boolean result = restTemplate.exchange(PRODUCT_ADD_URL, HttpMethod.POST,new HttpEntity<Object>(product,httpHeaders), Boolean.class).getBody();
-        return  result;
+//        Boolean result = restTemplate.exchange(PRODUCT_ADD_URL, HttpMethod.POST,new HttpEntity<Object>(product,httpHeaders), Boolean.class).getBody();
+//        return  result;
+        return  iProductClientService.addPorduct(product);
     }
 }
